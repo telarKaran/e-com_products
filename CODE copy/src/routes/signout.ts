b1776ffc23e400fb41c4e1e9f21394d264525d77
TelarKaran <telarkaran@outlook.com>
2025-02-21 01:43:32 +0530

    deleted accedental copy of auth service

--- CODE copy/src/routes/signout.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { signout } from '../controllers/authController';
-
-const router = express.Router();
-
-router.post('/api/users/signout', signout);
-
-export { router as signoutRouter };
